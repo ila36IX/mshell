61cf6deaef8dbb2594a611bfa8fd46d858191c10
sboukiou <sboukiou@student.1337.ma>
2025-05-15 13:42:58 +0100

    Added ANSI color sequences

--- includes.h
+++ includes.h
@@ -6,7 +6,7 @@
 /*   By: sboukiou <sboukiou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 13:23:19 by sboukiou          #+#    #+#             */
-/*   Updated: 2025/05/15 13:29:02 by sboukiou         ###   ########.fr       */
+/*   Updated: 2025/05/15 13:39:59 by sboukiou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,12 @@
 # include <string.h>
 
 # define MAX_REDIRECTIONS 200
+# define RESET "\033[0m"
+# define BRED "\033[1;31m"
+# define BACKGROUND_RED "\e[41m"
+# define BGREEN "\033[1;32m"
+# define BYELLOW "\033[1;33m"
+# define BBLUE "\033[1;34m"
+# define UPURPLE "\e[4;35m"
 
 #endif
