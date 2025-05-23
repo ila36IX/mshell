c6515e5a55aea54de6d43db22565109efae543f0
ila36ix <jbariali002@gmail.com>
2025-05-23 11:54:50 +0100

    Add todo utility

---
+++ debug_utils.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * todo - Indicates unfinished code
+ *
+ * @msg: What you need to implement later
+ * @arg: more details or NULL
+ *
+ * Return: Nothing
+ */
+void todo(char *msg, char *arg)
+{
+	printf("TODO: %s", msg);
+	if (arg)
+		printf(" (%s)", arg);
+	printf("\n");
+	exit(1);
+}
