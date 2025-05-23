c971d80e62bda4c36059e68a00421932273cd8d6
ila36ix <jbariali002@gmail.com>
2025-05-23 19:42:19 +0100

    Chnage waite space field name

--- lexer.h
+++ lexer.h
@@ -4,16 +4,16 @@
 
 typedef enum {
     TOKEN_NULL = 0,
-    // TOKEN_IN,
-    // TOKEN_OUT,
-    // TOKEN_APPEND,
-    // TOKEN_HEREDOC,
-    // TOKEN_OR,
-    // TOKEN_AND,
+    TOKEN_IN,
+    TOKEN_OUT,
+    TOKEN_APPEND,
+    TOKEN_HEREDOC,
+    TOKEN_OR,
+    TOKEN_AND,
     TOKEN_PIPE,
-    // TOKEN_SQ,
-    // TOKEN_DQ,
-    // TOKEN_WORD,
+    TOKEN_SQ,
+    TOKEN_DQ,
+    TOKEN_WORD,
     TOKEN_INVALID,
 } t_token_kind;
 
@@ -35,12 +35,12 @@ typedef struct s_token {
     t_token_kind kind;
     const char *text;
     size_t text_len;
-    bool ws_before; /* there is white space before this token */
+    bool whitespace_before; /* is this token have a white space before it */
 } t_token;
 
 t_lexer lexer_new(const char *content, size_t size);
 t_token lexer_next_token(t_lexer *l);
 const char *token_kind_name(t_token_kind kind);
-void	print_token(t_token kind);
+void	print_token(t_token token);
 
 #endif /*_LEXER_H_ */
