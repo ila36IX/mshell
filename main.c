7a2ea1e21fae3ca8e2d6ec85d1473adc80e729dc
sboukiou <sboukiou@student.1337.ma>
2025-05-27 19:12:20 +0100

    Moved builtins into exectuion dir

--- main.c
+++ main.c
@@ -10,27 +10,43 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "main.h"
+#include "./main.h"
+
+# define PROMPT BYELLOW"[sboukiou@debian ~]"BBLUE"$ "RESET
+
+
+/**
+	 * get_cmd_type - Returns the type a builtin command based on a string
+	 * @: string to check
+	 * Return: Type of builtin corresponding or INVALID otherwise
+ */
+
+t_builtin_type	get_cmd_type(const char *cmd)
+{
+	if (ft_strcmp(cmd, "echo") == 0)
+		return (ECHO);
+	if (ft_strcmp(cmd, "exit") == 0)
+		return (EXIT);
+	return (INVALID);
+}
+
+/**
+	* main - The main function - Entry point of the program
+	* @argc: Number of command-line provided arguments for the program
+	* @argv: Strings-array of arguments
+	* @envp: Strings-array of environment variables
+	* Return: EXIT_SUCCESS (0) if succeeded | A costum failure status is used otherwise
+*/
 
 int	main(void)
 {
 	char *cmd;
-	t_lexer lexer;
-	t_token token;
-
-	while (1) {
-		cmd = readline("$ ");
-
-		if (!cmd || ft_strcmp(cmd, "exit") == 0)
-			break;
-		lexer = lexer_new(cmd, ft_strlen(cmd));
-		token = lexer_next_token(&lexer);
-		while (token.kind)
-		{
-			print_token(token);
-			token = lexer_next_token(&lexer);
-		}
+	do {
+		cmd = readline(PROMPT);
+		echo(cmd, true);
 		free(cmd);
-	}
+	} while (cmd != NULL);
+	if (cmd)
+		free(cmd);
 	return (EXIT_SUCCESS);
 }
