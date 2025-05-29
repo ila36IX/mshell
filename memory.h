518425e69bd814d8e7e229986a534559bb97b502
sboukiou <sboukiou@student.1337.ma>
2025-05-29 10:54:35 +0100

    Added prototypes for the ft_malloc && ft_gcadd_back functions

---
+++ memory.h
@@ -0,0 +1,9 @@
+#ifndef MEMORY_H
+# define MEMORY_H
+
+# include "./types.h"
+
+void	*ft_malloc(size_t	size, size_t unit);
+void	ft_gcadd_back(void *addr);
+void	*ft_malloc(size_t	size, size_t unit);
+#endif /* MEMORY_H */
