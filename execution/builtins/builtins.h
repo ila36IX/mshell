c13ae3ec1eb3bc158ad4a3d915cfc08a73429120
sboukiou <sboukiou@student.1337.ma>
2025-05-30 11:06:37 +0100

    Addin new env-utils (insert and rmove for use with export/unset)

--- execution/builtins/builtins.h
+++ execution/builtins/builtins.h
@@ -14,6 +14,9 @@ typedef struct s_env_dict
 int	echo(const char *text, bool newline);
 int	pwd(void);
 int	cd(int ac, char **av);
+/* Env utils-funcs */
 int	env(char **envp);
+static void	env_insert(t_list **head, char *key, char *value);
+static void	env_remove(t_list *head, char *key);
 
 #endif
--- execution/builtins/env.c
+++ execution/builtins/env.c
@@ -1,6 +1,6 @@
 # include "./builtins.h"
 
-void	env_init(t_list **head, char **env)
+static void	env_init(t_list **head, char **env)
 {
 	int	i;
 	t_env_dict	*dict;
@@ -19,8 +19,39 @@ void	env_init(t_list **head, char **env)
 		node = ft_lstnew(dict);
 		ft_gcadd_back(node);
 		ft_lstadd_back(head, node);
-		/*gc_clear_node(list, ft_split_free);*/
+		/*ft_g_cremove(list);*/
 		i++;
+	}
+}
+
+static void	env_insert(t_list **head, char *key, char *value)
+{
+	t_env_dict	*new_dict;
+	t_list		*node;
+
+	new_dict = ft_malloc(1, sizeof(t_env_dict));
+	new_dict->key = key;
+	new_dict->val = value;
+	node = ft_lstnew(&new_dict);
+	if (!node)
+		ft_gc_clear();
+	ft_gcadd_back(node);
+	ft_lstadd_back(head, node);
+}
+
+static void	env_remove(t_list *head, char *key)
+{
+	t_list	*temp;
+	t_env_dict	*dict;
+
+	temp = head;
+	while (head)
+	{
+		temp = head;
+		dict = (t_env_dict *)temp->content;
+		if (ft_strcmp(dict->key, key) == 0)
+			/*ft_lstdelone(temp, ft_gc_remove);*/
+		head = head->next;
 	}
 }
 
