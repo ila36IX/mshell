55094f57a7aeca5eff98cb0159933ecc7772747c
ila36IX <jbariali002@gmail.com>
2025-06-01 04:20:36 +0100

    ast print utils

---
+++ ast_type_to_str.c
@@ -0,0 +1,62 @@
+#include "print_ast.h"
+
+/**
+ * ast_type_to_str - Get string representatoin of a ast type
+ *
+ * @type: ast type to be represented
+ * Return: constant LATERAL string
+ */
+const char	*ast_type_to_str(t_ast_type type) 
+{
+	if (type == AST_CONNECTOR)
+		return ("CONNECTOR");
+	else if (type == AST_SUBSHELL)
+		return ("SUBSHELL");
+	else if (type == AST_SIMPLE_COMMAND)
+		return ("SIMPLE COMMAND");
+	else {
+		UNREACHABLE("ast type string represenation needed!");
+	}
+}
+
+/**
+ * connector_type_to_str - Get string representatoin of a connector type
+ *
+ * @type: connector type to be represented
+ * Return: constant LATERAL string
+ */
+const char *connector_type_to_str(t_connector type)
+{
+	if (type == CONNECTOR_AND)
+		return ("AND");
+	else if (type == CONNECTOR_OR)
+		return ("OR");
+	else if (type == CONNECTOR_PIPE)
+		return ("PIPE");
+	else
+	{
+		UNREACHABLE("Unknown connector type!");
+	}
+}
+
+/**
+ * redir_type_to_str - Get string representatoin of a redirection type
+ *
+ * @type: redicretion type to be represented
+ * Return: constant LATERAL string
+ */
+const char *redir_type_to_str(t_redirect_type type)
+{
+	if (type == REDIR_TYPE_IN)
+		return ("<");
+	else if (type == REDIR_TYPE_OUT)
+		return (">");
+	else if (type == REDIR_TYPE_HEREDOC)
+		return (">>");
+	else if (type == REDIR_TYPE_APPEND)
+		return (">>");
+	else
+	{
+		UNREACHABLE("Unknown redirect type!");
+	}
+}
---
+++ print_ast_utils.c
@@ -0,0 +1,86 @@
+#include "print_ast.h"
+
+/**
+ * _print_ast_helper - print ast nodes and follow its childs recursively meant only to be
+ * used internally with the ast printer
+ *
+ * @indent: current indentation
+ * @ast: first ast node of the tree
+ * Return: Nothing
+ */
+void _print_ast_helper(t_ast *ast, int indent)
+{
+	while (ast)
+	{
+		if (ast->type == AST_SIMPLE_COMMAND)
+			print_ast_simple_cmd(ast, indent);
+		else if (ast->type == AST_CONNECTOR)
+			print_ast_connector(ast, indent);
+		else if (ast->type == AST_SUBSHELL)
+			print_ast_subshell(ast, indent);
+		ast = ast->next;
+	}
+}
+
+/**
+ * _tree_line_prefix - print the line prefixes (tree lines), meant only to be
+ * used internally with the ast printer
+ *
+ * @indent: current indentation
+ * @is_empty_line: is there something to be added before this line ended or not
+ * Return: Nothing
+ */
+void	_tree_line_prefix(int indent, bool is_empty_line)
+{
+	int i;
+
+	i = 0;
+	while (i < indent)
+	{
+		if (i + 1 == indent && is_empty_line == false)
+			printf("%*s├", INDENT_SIZE, "");
+		else
+			printf("%*s│", INDENT_SIZE, "");
+		i++;
+	}
+	if (is_empty_line)
+		printf("\n");
+}
+
+/**
+ * _print_tree_end_root - prints the end root of ended node tree, meant only to
+ * be used internally with the ast printer
+ *
+ * @indent: current indentation
+ * Return: Nothing
+ */
+void	_print_tree_end_root(int indent)
+{
+	int i;
+
+	i = 0;
+	while (i < indent)
+	{
+		printf("%*s│", INDENT_SIZE, "");
+		i++;
+	}
+	printf("%*s╰", INDENT_SIZE, "");
+	printf("\n");
+}
+
+/**
+ * print_ast_connector - print ast node of type connector
+ *
+ * @ast: Pointer to ast node
+ * @indent: give it 0 if you're not printing a tree
+ * Return: Noting
+ */
+void	print_ast_connector(t_ast *ast, int indent)
+{
+	if (ast->type != AST_CONNECTOR)
+	{
+		UNREACHABLE("takes only none-null ast of connector type!");
+	}
+	print_ast_type(ast, indent++);
+	_tree_line_prefix(indent, true);
+}
