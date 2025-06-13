dc27b37caf53e88cad9a0e69f78ca92b48704d98
ila36ix <jbariali002@gmail.com>
2025-06-13 11:40:41 +0100

    Environ utils

---
+++ xstrdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/*
+ * xstrdup - allocate duplication of string wihtout using gc
+ *
+ * @src: String to be duplicated
+ *
+ * Return: Pointer to allocated string
+ **/
+char	*xgc_strdup(const char *src)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	while (src[i])
+		i++;
+	str = malloc(sizeof(char) * (i + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (*src)
+		str[i++] = *src++;
+	str[i] = '\0';
+	return (str);
+}
+
