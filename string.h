28369a41d74d786bc95c976912dc676d19c77571
ila36ix <jbariali002@gmail.com>
2025-06-16 14:40:47 +0100

    Dynamic string array

---
+++ string.h
@@ -0,0 +1,15 @@
+#include "main.h"
+
+#define INIT_STR_ARRAY_SIZE 16
+typedef struct s_string
+{
+	size_t	length;
+	size_t	capacity;
+	char	*buff;
+}			t_string;
+
+void		string_append_char(t_string *s, char c);
+void		string_append_str(t_string *s, const char *str_to_append,
+				size_t size);
+t_string	string_init(void);
+void		string_append_cstr(t_string *s, const char *str_to_append);
