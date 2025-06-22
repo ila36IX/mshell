29831fc086002df5cc7c2225e1a01956ededb1dd
sboukiou <sboukiou@student.1337.ma>
2025-06-22 12:42:26 +0100

    Main.c

--- main.c
+++ main.c
@@ -1,1 +1,19 @@
+# include "./main.h"
+# include "./parser.h"
 
+
+int	main(int ac, char **av, char **envp)
+{
+	t_ast	*ast;
+	char	*line;
+	t_lexer	lexer;
+
+	(void)ac;
+	(void)av;
+	while ((line = readline("$ ")))
+	{
+		lexer = lexer_new(line, ft_strlen(line));
+		ast = create_ast(&lexer);
+		exec_main(ast, envp);
+	}
+}
