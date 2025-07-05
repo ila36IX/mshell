1e4b37235e4973a97b818a04342e4116f85a4ce1
ila36ix <jbariali002@gmail.com>
2025-07-05 17:12:40 +0100

    ADD: dynamic array helpers for argv data structure

--- dynamic_array/arrays.h
+++ dynamic_array/arrays.h
@@ -6,28 +6,18 @@
 /*   By: aljbari <jbariali002@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 17:42:16 by aljbari           #+#    #+#             */
-/*   Updated: 2025/07/03 17:42:18 by aljbari          ###   ########.fr       */
+/*   Updated: 2025/07/05 16:48:45 by aljbari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAYS_H
 # define ARRAYS_H
-
 # include "../includes.h"
+# include "../types.h"
 
-typedef struct s_word
-{
-	const char	*text;
-	size_t		len;
-}				t_word;
-
-typedef struct s_words
-{
-	t_word		*buff;
-	size_t		capacity;
-	size_t		length;
-}				t_words;
 void			words_append(t_words *words, t_word word);
 t_words			words_init(void);
+void	args_append(t_simple_cmd *scmd, char *arg);
+void	init_argv(t_simple_cmd *scmd);
 
 #endif
