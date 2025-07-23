113d3a28abf08ce2be1d91f40583a9abb44b9217
sboukiou <sboukiou@student.1337.ma>
2025-07-23 21:01:55 +0100

    Forking in case of a pipe

--- exec/exec_builtin.c
+++ exec/exec_builtin.c
@@ -7,26 +7,41 @@ int	exec_builtin(t_ast *ast)
 {
 	char	**av;
 	int		ac;
+	int		pid;
+	int		status;
 
 	if (ast == NULL)
 		return (ERR_NULL);
 	if (ast->simple_cmd.argv == NULL)
 		return (ERR_NULL);
-	av = ast->simple_cmd.argv;
-	ac = ast->simple_cmd.argc;
-	if (ft_strcmp(av[0], "echo") == SUCCESS)
-		return (ft_echo(ac, av));
-	if (ft_strcmp(av[0], "cd") == SUCCESS)
-		return (ft_cd(ac, av));
-	if (ft_strcmp(av[0], "pwd") == SUCCESS)
-		return (ft_pwd(ac, av));
-	if (ft_strcmp(av[0], "export") == SUCCESS && !is_pipe_next(ast))
-		return (ft_export(ac, av));
-	if (ft_strcmp(av[0], "unset") == SUCCESS)
-		return (ft_unset(ac, av));
-	if (ft_strcmp(av[0], "env") == SUCCESS)
-		return (ft_env(ac, av));
-	if (ft_strcmp(av[0], "exit") == SUCCESS)
-		return (ft_exit(ac, av));
+	pid = 0;
+	status = 0;
+	if (is_pipe_next(ast) == true)
+		pid = fork();
+	if (pid == 0)
+	{
+		av = ast->simple_cmd.argv;
+		ac = ast->simple_cmd.argc;
+		if (ft_strcmp(av[0], "echo") == SUCCESS)
+			status = ft_echo(ac, av);
+		if (ft_strcmp(av[0], "cd") == SUCCESS)
+			status = ft_cd(ac, av);
+		if (ft_strcmp(av[0], "pwd") == SUCCESS)
+			status = ft_pwd(ac, av);
+		if (ft_strcmp(av[0], "export") == SUCCESS && !is_pipe_next(ast))
+			status = ft_export(ac, av);
+		if (ft_strcmp(av[0], "unset") == SUCCESS)
+			status = ft_unset(ac, av);
+		if (ft_strcmp(av[0], "env") == SUCCESS)
+			status = ft_env(ac, av);
+		if (ft_strcmp(av[0], "exit") == SUCCESS)
+			status = ft_exit(ac, av);
+		if (is_pipe_next(ast) == true)
+			exit(status);
+		else
+			return (status);
+	}
+	else
+		pid_push(pid);
 	return (SUCCESS);
 }
